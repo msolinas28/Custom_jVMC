f9d7530f7309102123fb42ccbf8344648d066017
msolinas <massimosolinas28@gmail.com>
2026-02-18 11:38:34 +0100

    -

--- tests/tdvp_test.py
+++ tests/tdvp_test.py
@@ -46,76 +46,7 @@ from jVMC_exp.util import measure
 
     #         self.assertTrue(jnp.max(jnp.abs((obs['energy']['mean'] - exE) / exE)) < 1e-3)
 
-class TestTimeEvolution(unittest.TestCase):
-    def test_time_evolution(self):
-        L = 4
-        J = -1.0
-        hx = -0.3
-
-        weights = jnp.array(
-            [0.23898957, 0.12614753, 0.19479055, 0.17325271, 0.14619853, 0.21392751,
-             0.19648707, 0.17103704, -0.15457255, 0.10954413, 0.13228065, -0.14935214,
-             -0.09963073, 0.17610707, 0.13386381, -0.14836467]
-        )
-
-        # Set up variational wave function
-        rbm = nets.CpxRBM(numHidden=2, bias=False)
-        psi = NQS(rbm, L, 2 ** L)
-        psi.parameters = weights
-
-        # Set up exact sampler
-        exactSampler = sampler.ExactSampler(psi)
-
-        # Set up hamiltonian for time evolution
-        hamiltonian = 0
-        for l in range(L):
-            hamiltonian += J * op.SigmaZ(l) * op.SigmaZ((l + 1) % L) + hx * op.SigmaX(l)
-
-        # Set up ZZ observable
-        ZZ = 0
-        for l in range(L):
-            ZZ += op.SigmaZ(l) * op.SigmaZ((l + 1) % L)
-
-        # Set up adaptive time stepper
-        stepper = jVMCstepper.AdaptiveHeun(timeStep=1e-3, tol=1e-5)
-        tdvpEquation = jVMC_exp.util.TDVP(exactSampler, snrTol=1, pinvTol=0.0, pinvCutoff=1e-8, rhsPrefactor=1.j, diagonalShift=0., makeReal='imag')
-
-        t = 0
-        t_max = 0.5
-        obs = []
-        times = []
-        times.append(t)
-        newMeas = measure({'E': hamiltonian, 'ZZ': ZZ}, psi, exactSampler)
-        obs.append([newMeas['E']['mean'], newMeas['ZZ']['mean']])
-
-        pbar = tqdm.tqdm(total=t_max)
-        while t < t_max:
-            psi.parameters, dt = stepper.step(0, tdvpEquation, psi.parameters_flat, hamiltonian=hamiltonian, psi=psi)
-            t += dt
-            times.append(t)
-            newMeas = measure({'E': hamiltonian, 'ZZ': ZZ}, psi, exactSampler)
-            obs.append([newMeas['E']['mean'], newMeas['ZZ']['mean']])
-            pbar.update(float(dt))
-
-        obs = np.array(obs)
-
-        # Check energy conservation
-        obs[:, 0] = np.abs((obs[:, 0] - obs[0, 0]) / obs[0, 0])
-        self.assertTrue(np.max(obs[:, 0]) < 1e-3)
-
-        # Check observable dynamics
-        zz = interp1d(np.array(times), obs[:, 1])
-        refTimes = np.arange(0, 0.5, 0.05)
-        netZZ = zz(refTimes)
-        refZZ = np.array(
-            [0.882762129306284, 0.8936168721790617, 0.9257753299594491, 0.9779836185039352, 1.0482156449061142,
-             1.1337654450614298, 1.231369697427413, 1.337354107391303, 1.447796176316155, 1.558696104640795,
-             1.666147269524912, 1.7664978782554912, 1.8564960156892512, 1.9334113379450693, 1.9951280521882777,
-             2.0402054805651546, 2.067904337137255, 2.078178742959828, 2.071635856483114, 2.049466698269522, 2.049466698269522]
-        )
-        self.assertTrue(np.max(np.abs(netZZ - refZZ[:len(netZZ)])) < 1e-3)
-
-# class TestTimeEvolutionMCSampler(unittest.TestCase):
+# class TestTimeEvolution(unittest.TestCase):
 #     def test_time_evolution(self):
 #         L = 4
 #         J = -1.0
@@ -129,52 +60,53 @@ class TestTimeEvolution(unittest.TestCase):
 
 #         # Set up variational wave function
 #         rbm = nets.CpxRBM(numHidden=2, bias=False)
-#         psi = NQS(rbm, batchSize=5000)
-#         psi(jnp.array([[[1, 1, 1, 1]]]))
-#         psi.set_parameters(weights)
-
-#         # Set up hamiltonian for time evolution
-#         hamiltonian = op.BranchFreeOperator()
-#         for l in range(L):
-#             hamiltonian.add(op.scal_opstr(J, (op.Sz(l), op.Sz((l + 1) % L))))
-#             hamiltonian.add(op.scal_opstr(hx, (op.Sx(l), )))
-
-#         # Set up ZZ observable
-#         ZZ = op.BranchFreeOperator()
-#         for l in range(L):
-#             ZZ.add((op.Sz(l), op.Sz((l + 1) % L)))
+#         psi = NQS(rbm, L, 2 ** L)
+#         psi.parameters = weights
 
 #         # Set up exact sampler
-#         MCsampler = sampler.MCSampler(psi, (L,), jax.random.PRNGKey(0), numSamples=50000, updateProposer=sampler.propose_spin_flip, mu=1, numChains=500)
+#         exactSampler = sampler.ExactSampler(psi)
+
+#         # Set up hamiltonian for time evolution
+#         hamiltonian = 0
+#         for l in range(L):
+#             hamiltonian += J * op.SigmaZ(l) * op.SigmaZ((l + 1) % L) + hx * op.SigmaX(l)
+
+#         # Set up ZZ observable
+#         ZZ = 0
+#         for l in range(L):
+#             ZZ += op.SigmaZ(l) * op.SigmaZ((l + 1) % L)
 
 #         # Set up adaptive time stepper
-#         stepper = jVMCstepper.AdaptiveHeun(timeStep=1e-3, tol=1e-4)
-
-#         tdvpEquation = jVMC_exp.util.TDVP(MCsampler, snrTol=1, pinvTol=1e-8, rhsPrefactor=1.j, diagonalShift=0., makeReal='imag', crossValidation=True)
+#         stepper = jVMCstepper.AdaptiveHeun(timeStep=1e-3, tol=1e-5)
+#         tdvpEquation = jVMC_exp.util.TDVP(exactSampler, snrTol=1, pinvTol=0.0, pinvCutoff=1e-8, rhsPrefactor=1.j, diagonalShift=0., makeReal='imag')
 
 #         t = 0
+#         t_max = 0.5
 #         obs = []
 #         times = []
 #         times.append(t)
-#         newMeas = measure({'E': hamiltonian, 'ZZ': ZZ}, psi, MCsampler)
+#         newMeas = measure({'E': hamiltonian, 'ZZ': ZZ}, psi, exactSampler)
 #         obs.append([newMeas['E']['mean'], newMeas['ZZ']['mean']])
-#         while t < 0.2:
-#             dp, dt = stepper.step(0, tdvpEquation, psi.get_parameters(), hamiltonian=hamiltonian, psi=psi, numSamples=5000)
-#             psi.set_parameters(dp)
-#             t += dt
-#             times.append(t)
-#             newMeas = measure({'E': [(hamiltonian, t)], 'ZZ': ZZ}, psi, MCsampler)
-#             obs.append([newMeas['E']['mean'], newMeas['ZZ']['mean']])
 
-#         obs = np.array(jnp.asarray(obs))
+#         pbar = tqdm.tqdm(total=t_max)
+        # while t < t_max:
+        #     psi.parameters, dt = stepper.step(0, tdvpEquation, psi.parameters_flat, hamiltonian=hamiltonian, psi=psi)
+        #     t += dt
+        #     times.append(t)
+        #     newMeas = measure({'E': hamiltonian, 'ZZ': ZZ}, psi, exactSampler)
+        #     obs.append([newMeas['E']['mean'], newMeas['ZZ']['mean']])
+        #     pbar.update(float(dt))
+        # pbar.close()
+
+#         obs = np.array(obs)
 
 #         # Check energy conservation
 #         obs[:, 0] = np.abs((obs[:, 0] - obs[0, 0]) / obs[0, 0])
-#         self.assertTrue(np.max(obs[:, 0]) < 1e-1)
+#         self.assertTrue(np.max(obs[:, 0]) < 1e-3)
 
 #         # Check observable dynamics
-#         zz = interp1d(np.array(times), obs[:, 1, 0])
-#         refTimes = np.arange(0, 0.2, 0.05)
+#         zz = interp1d(np.array(times), obs[:, 1])
+#         refTimes = np.arange(0, 0.5, 0.05)
 #         netZZ = zz(refTimes)
 #         refZZ = np.array(
 #             [0.882762129306284, 0.8936168721790617, 0.9257753299594491, 0.9779836185039352, 1.0482156449061142,
@@ -182,7 +114,82 @@ class TestTimeEvolution(unittest.TestCase):
 #              1.666147269524912, 1.7664978782554912, 1.8564960156892512, 1.9334113379450693, 1.9951280521882777,
 #              2.0402054805651546, 2.067904337137255, 2.078178742959828, 2.071635856483114, 2.049466698269522, 2.049466698269522]
 #         )
-#         self.assertTrue(np.max(np.abs(netZZ - refZZ[:len(netZZ)])) < 2e-2)
+#         self.assertTrue(np.max(np.abs(netZZ - refZZ[:len(netZZ)])) < 1e-3)
+
+class TestTimeEvolutionMCSampler(unittest.TestCase):
+    def test_time_evolution(self):
+        L = 4
+        J = -1.0
+        hx = -0.3
+
+        num_samples = 2 ** 16
+        num_chains = 2 ** 14
+        batch_size = 2 ** 16
+
+        weights = jnp.array(
+            [0.23898957, 0.12614753, 0.19479055, 0.17325271, 0.14619853, 0.21392751,
+             0.19648707, 0.17103704, -0.15457255, 0.10954413, 0.13228065, -0.14935214,
+             -0.09963073, 0.17610707, 0.13386381, -0.14836467]
+        )
+
+        # Set up variational wave function
+        rbm = nets.CpxRBM(numHidden=2, bias=False)
+        psi = NQS(rbm, L, batch_size)
+        psi.parameters = weights
+
+        # Set up hamiltonian for time evolution
+        hamiltonian = 0
+        for l in range(L):
+            hamiltonian += J * op.SigmaZ(l) * op.SigmaZ((l + 1) % L) + hx * op.SigmaX(l)
+
+        # Set up ZZ observable
+        ZZ = 0
+        for l in range(L):
+            ZZ += op.SigmaZ(l) * op.SigmaZ((l + 1) % L)
+
+        # Set up exact sampler
+        proposer = jVMC_exp.propose.SpinFlip()
+        mc_sampler = sampler.MCSampler(psi, proposer, 0, num_chains, num_samples, mu=1)
+
+        # Set up adaptive time stepper
+        stepper = jVMCstepper.AdaptiveHeun(timeStep=1e-3, tol=1e-4)
+
+        tdvpEquation = jVMC_exp.util.TDVP(mc_sampler, snrTol=1, pinvTol=1e-8, rhsPrefactor=1.j, diagonalShift=0., makeReal='imag', crossValidation=True)
+
+        t = 0
+        t_max = 0.2
+        obs = []
+        times = []
+        times.append(t)
+        newMeas = measure({'E': hamiltonian, 'ZZ': ZZ}, psi, mc_sampler)
+        obs.append([newMeas['E']['mean'], newMeas['ZZ']['mean']])
+        pbar = tqdm.tqdm(total=t_max)
+        while t < t_max:
+            psi.parameters, dt = stepper.step(0, tdvpEquation, psi.parameters_flat, hamiltonian=hamiltonian, psi=psi)
+            t += dt
+            times.append(t)
+            newMeas = measure({'E': hamiltonian, 'ZZ': ZZ}, psi, mc_sampler)
+            obs.append([newMeas['E']['mean'], newMeas['ZZ']['mean']])
+            pbar.update(float(dt))
+        pbar.close()
+
+        obs = np.array(obs)
+
+        # Check energy conservation
+        obs[:, 0] = np.abs((obs[:, 0] - obs[0, 0]) / obs[0, 0])
+        self.assertTrue(np.max(obs[:, 0]) < 1e-1)
+
+        # Check observable dynamics
+        zz = interp1d(np.array(times), obs[:, 1])
+        refTimes = np.arange(0, 0.2, 0.05)
+        netZZ = zz(refTimes)
+        refZZ = np.array(
+            [0.882762129306284, 0.8936168721790617, 0.9257753299594491, 0.9779836185039352, 1.0482156449061142,
+             1.1337654450614298, 1.231369697427413, 1.337354107391303, 1.447796176316155, 1.558696104640795,
+             1.666147269524912, 1.7664978782554912, 1.8564960156892512, 1.9334113379450693, 1.9951280521882777,
+             2.0402054805651546, 2.067904337137255, 2.078178742959828, 2.071635856483114, 2.049466698269522, 2.049466698269522]
+        )
+        self.assertTrue(np.max(np.abs(netZZ - refZZ[:len(netZZ)])) < 2e-2)
 
 
 # class TestSNRConsistency(unittest.TestCase):
