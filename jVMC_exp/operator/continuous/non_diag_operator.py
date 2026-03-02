6cce7f499eddf8e8cef2737be4f3cb4228cd1b39
msolinas28 <massimosolinas28@gmail.com>
2026-03-02 18:14:24 +0100

    -

--- jVMC_exp/operator/continuous/non_diag_operator.py
+++ jVMC_exp/operator/continuous/non_diag_operator.py
@@ -49,4 +49,4 @@ class TotalKineticOperator(Operator):
         grad_log_psi = grad_log_psi(s)
         laplacian_psi = lap_log_psi + grad_log_psi ** 2
 
-        return - 0.5 * jnp.sum(laplacian_psi * self._inverse_mass).real
+        return - 0.5 * jnp.sum(laplacian_psi * self._inverse_mass)
