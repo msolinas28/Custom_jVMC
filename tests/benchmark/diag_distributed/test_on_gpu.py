3861c0f87aecefc07a41f34098fa6075e0cc93a4
msolinas28 <massimosolinas28@gmail.com>
2026-08-07 10:36:08 +0200

    -

--- tests/benchmark/diag_distributed/test_on_gpu.py
+++ tests/benchmark/diag_distributed/test_on_gpu.py
@@ -1,16 +1,17 @@
-import jax
-import jax.numpy as jnp
-import argparse
-
-print("Devices =", jax.devices())
-print("Device count =", jax.device_count())
-
-import math
 from jVMC_exp.solver.util import diagonalize
 from jVMC_exp.sharding_config import MESH_2D
 
+import jax
+import jax.numpy as jnp
+import argparse
+import math
+
+if jax.process_index() == 0:
+    print("Devices =", jax.devices())
+    print("Device count =", jax.device_count())
+
 parser = argparse.ArgumentParser()
-parser.add_argument("N")
+parser.add_argument("N", type=int)
 args = parser.parse_args()
 
 N = args.N
@@ -27,10 +28,12 @@ A = jnp.pad(A, ((0, pad_size), (0, pad_size)), mode="constant")
 p_rows, p_cols = MESH_2D.shape["row"], MESH_2D.shape["col"]
 T_A = math.gcd((A.shape[0]) // p_rows, (A.shape[0]) // p_cols)
 
-print(f"N={N} padded to {N_padded} (+{pad_size}), {m} devices -> {p_rows}x{p_cols} grid, T_A={T_A}")
+if jax.process_index() == 0:
+    print(f"N={N} padded to {N_padded} (+{pad_size}), {m} devices -> {p_rows}x{p_cols} grid, T_A={T_A}")
 
 ev, V = diagonalize(A, pad_size, mode='distributed')
 
-print('Matrix original size: ', N)
-print(f'Eigenvalues: \n Shape={ev.shape} \n Shards={ev.addressable_shards}')
-print(f'Eigenvectors: \n Shape={V.shape} \n Shards={V.addressable_shards}')
+if jax.process_index() == 0:
+    print('Matrix original size: ', N)
+    print(f'Eigenvalues: \n Shape={ev.shape} \n Shards={ev.addressable_shards}')
+    print(f'Eigenvectors: \n Shape={V.shape} \n Shards={V.addressable_shards}')
